44b204a5dc9f3edd8845bf45eb2e15fb6f722630
Stacy <stacy1burris@gmail.com>
2021-11-19 15:30:02 -0800

    feat: 🎸 function annotation

---
+++ features/annotations/functions.ts
@@ -0,0 +1,26 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => { // (): number = annotation
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+};
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
